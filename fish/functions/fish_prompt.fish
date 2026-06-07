94aefdefc74d4489b9cb484bf684fab26874b78e
kiyo66 <ogawatandf@icloud.com>
2026-06-08 08:52:26 +0900

    Modify left misalignment in the CLI.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,31 +1,50 @@
 #!/usr/bin/env fish
 
 function __current_path
-  set pwd_result (pwd)
+  set -l pwd_result (pwd)
+
   if test (string length $pwd_result) -gt (math $COLUMNS - 10)
-	  echo -n (set_color --bold blue) (prompt_pwd) (set_color normal) 
+    printf "%s%s%s" (set_color --bold blue) (prompt_pwd) (set_color normal)
   else
-	  echo -n (set_color --bold blue) $pwd_result (set_color normal) 
+    printf "%s%s%s" (set_color --bold blue) $pwd_result (set_color normal)
   end
 end
 
+function __user_host
+  printf "%s|  %s%s%s" \
+    (set_color white) \
+    (set_color --bold cyan) \
+    (hostname) \
+    (set_color normal)
+end
+
 function fish_prompt
+  set -l last_status $status
+  set -l status_face
+
   set -g fish_prompt_git_status_staged "o"
-  set -g fish_prompt_git_status_conflicted 'x'
-  set -g fish_prompt_git_status_changed '+'
+  set -g fish_prompt_git_status_conflicted "x"
+  set -g fish_prompt_git_status_changed "+"
   set -g fish_prompt_git_status_untracked "?"
   set -g fish_prompt_git_status_clean "✔"
 
-  if [ $status -eq 0 ]
+  if test $last_status -eq 0
     set status_face (set_color brgreen --bold)"*(·_·) >> \$ "
   else
     set status_face (set_color brblue --bold)"'(;-;) >> \$ "
   end
 
-  echo -n (set_color white)"╭─"(set_color normal)
-#   __user_host
+  printf "%s╭─ %s" (set_color white) (set_color normal)
   __current_path
-#  __fish_git_prompt
-  echo -e ''
-  echo (set_color white)"╰─"$status_face(set_color normal)
+  __fish_git_prompt
+  printf "\n"
+
+  __user_host
+  printf "\n"
+
+  printf "%s╰─%s%s%s" \
+    (set_color white) \
+    (set_color normal) \
+    "$status_face" \
+    (set_color normal)
 end
